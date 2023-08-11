3fcf99f23adfcc1227c922f6a10aa6083295359f
Sjoerd van Dijk <sjoerd@smvd.net>
2023-08-11 10:09:34 +0200

    minor changes

--- SIP channel hangup/call_monitor.sh
+++ SIP channel hangup/call_monitor.sh
@@ -2,4 +2,6 @@
 #Sjoerd van Dijk 10-08-2023
 #Monitor ongoing calls, if call exceeds two hours. Send hangup request. 
 
-asterisk -rx "core show channels concise" | awk -F '!' '{print ($12 > 7200) ? "asterisk -rx \"channel request hangup "$1"\"" : "echo \""$1" "$12"\"" }' | bash
+#asterisk -rx "core show channels concise" | awk -F '!' '{print ($12 > 7200) ? "asterisk -rx \"channel request hangup "$1"\"" : "echo \""$1" "$12"\"" }' | bash
+
+asterisk -rx "core show channels concise" | awk -F '!' '{print ($12 > 7200) ? "asterisk -rx \"channel request hangup "$1"\"" : "echo \""$1" "$12"\"" }' | bash >> /var/log/call_monitor.log 2>&1
